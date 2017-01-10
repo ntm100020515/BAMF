74e714cdca21a7c6f6265df6742a5278e0e7274d
Will McLaughlin <mclaughlin.will24@gmail.com>
2017-01-10 13:22:40 -0600

    no message

---
+++ Capstone2/Assets/Character/CharacterMovement.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+    public float speed;
+
+	void Update () {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                transform.Translate(Vector2.right * (speed*2));
+            }
+            else
+            {
+                transform.Translate(Vector2.right * speed);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                transform.Translate(Vector2.left * (speed * 2));
+            }
+            else
+            {
+                transform.Translate(Vector2.left * speed);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                transform.Translate(Vector2.up * (speed * 2));
+            }
+            else
+            {
+                transform.Translate(Vector2.up * speed);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                transform.Translate(Vector2.down * (speed * 2));
+            }
+            else
+            {
+                transform.Translate(Vector2.down * speed);
+            }
+        }
+
+    }
+}
