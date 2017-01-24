275a1ff814555e6c7852dcad49e8f827dea98d0a
Noah Miles <miles.noaht@gmail.com>
2017-01-24 13:56:38 -0600

    1-24

---
+++ GateTest/Assets/TestOne.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TestOne : MonoBehaviour
+{
+    public GameObject
+        public static bool onoff = true;
+    private TestScript otherScript;
+    private bool a;
+    
+
+	// Use this for initialization
+	void Start ()
+    {
+        a = otherScript.touched; 
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        GameObject.Find("Thing").SetActive(onoff); 
+	}
+}
