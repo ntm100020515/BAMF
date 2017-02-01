38bd961c18449002d60d465c1c44a0ae3b905d57
Noah Miles <miles.noaht@gmail.com>
2017-02-01 13:17:16 -0600

    2.1.17

---
+++ MAP TEST/Assets/TurnOff.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class TurnOff : MonoBehaviour
+{
+    public Toucher ts1;
+
+	// Use this for initialization
+	void Start ()
+    {
+       
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (ts1.touched == true)
+        {
+            gameObject.SetActive(false);
+            
+        }
+    }
+}
